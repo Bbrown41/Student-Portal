116e4d1dfcd45315a13569060fbbbc14592b5ab2
Capstick <acapstick@students.blackhawk.edu>
2019-05-07 21:02:52 -0500

    added includes, added calendar, changed pages from html to php, fixed some minor css issues

--- index.php
+++ index.php
@@ -1,185 +1,115 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>BTC Flex Hub</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" type="image/png" href="images/favicon.ico"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="main.css">       
-    </head>
-    <body>
-        <header>
-            <!-- Top Navigation Bar -->
-            <div class="top-nav">
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://blackhawk.edu">BTC Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://mybtc.blackhawk.edu">MyBTC</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://blackhawk.blackboard.com">Blackboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://outlook.office.com/blackhawk.edu">Email</a>
-                    </li>
-                    <li>
-                        <button type="button" class="btn navbar-btn" style="margin-right: 0.4em;">Student Login ></button>
-                    </li>
-                    <li>
-                        <button type="button" class="btn navbar-btn">Staff Login ></button>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- Banner -->
-            <div id="banner">
-                <div id="banner-text">
-                    <h1>Blackhawk Technical College</h1>
-                    <h2>Flex Hub</h2>
-                </div>
-                <div id="banner-search">
-                    <form action="/#.php"><!--TO DO: Replace with actual php link once created -->
-                        <input type="text" placeholder="Search" name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-            </div>
-        </header>
-
-         <!-- Main Navigation -->
-         <nav class="navbar navbar-expand-sm sticky-top">
-                <a class="navbar-brand" href="#">
-                    <img src="images/btc-logo.png" alt="BTC Logo" width="45%";>
-                </a>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <span class="navbar-text">|</span>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Programs.html">FLEX Programs & Classes</a>
-                    </li>
-                    <span class="navbar-text">|</span>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Enrollment.html">Enrollment</a>
-                    </li>
-                    <span class="navbar-text">|</span>
-                    <li class="nav-item">
-                        <a class="nav-link" href="About.html">About FLEX</a>
-                    </li>
-                </ul>   
-            </nav>
-
+<?php
+    // Header
+    include("./includes/header.php");
+?>
         <!-- Main Content -->
         <main>
             <h2><b>Announcements & Upcoming Events</b></h2>
-
-            <?php
-
-                session_start();
-
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "hub";
-
-                // Create connection
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-
-                $sql = "
-                SELECT * 
-                FROM announcements
-                ORDER BY date_created DESC";
-                $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-                        echo "<div class='cards'>";
-                        while($row = $result->fetch_assoc()) {  // Need to change this to only show 10-20 most recent announcements.
-                            
-                            $title=$row["title"];
-                            $content=$row["content"];
-                            $date_created=date("M d, Y",strtotime($row["date_created"]));
-                            $start_date=$row["start_date"];
-                            $end_date=$row["end_date"];
-
-                            if($end_date>date("Y-m-d H:i:s")){
-                                echo "
-                                    <div class='card'>
-                                        <div class='container'>
-                                        <h3><b>$title</b></h3> 
-                                        <p><h4>$content</h4></p> 
-                                        <h5>Posted: $date_created</h5>
-                                        </div>
-                                    </div>
-                                ";
-                            }
-                        }
-                        echo "</div>";
-                    }
-                $conn->close();
-
-            ?>
-
-
-        </main>
-
-
-        <!-- Footer -->
-        <footer>
             <div class="row">
-                <!-- Name and Address -->
-                <div class="col" id="footer-left">
-                    <h1>Blackhawk Technical College</h1>
-                    <h2>6004 S County Road G, Janesville, WI 53546</h2>
-                </div>
 
-                <!-- Social Media Links & Contact Info -->
-                <div class="col" id="footer-middle">
-                    <span class="socialmedia">
-                        <a href="https://www.facebook.com/blackhawktech/">
-                            <i class="fa fa-facebook-square"></i>
-                        </a>
-                        <a href="https://twitter.com/blackhawk_tech">
-                            <i class="fa fa-twitter-square"></i>
-                        </a>
-                        <a href="https://www.instagram.com/blackhawktech/?hl=en">
-                            <i class="fa fa-instagram"></i>
-                        </a>
-                        <a href="https://www.linkedin.com/school/blackhawk-technical-college/">
-                            <i class="fa fa-linkedin-square"></i>
-                        </a>
-                        <a href="https://www.youtube.com/channel/UCA9LW5SgcsuW9p0nApelaZw">
-                            <i class="fa fa-youtube-square"></i>
-                        </a>
-                    </span>
-                    <p>
-                        <span class="contact"><a href="tel:6087586900">(608) 758-6900</a></span>
-                        <span class="contact"><a href="mailto:info@blackhawk.edu">info@blackhawk.edu</a></span>
-                    </p>
-                </div>
+                <!-- Announcements -->
+                <div class="col-sm-9">
+                    <?php
 
-                <!-- Search -->
-                <div class="col" id="footer-right">
-                    <div id="footer-search">
-                        <form action="/#.php"><!--TO DO: Replace with actual php link once created -->
-                            <input type="text" placeholder="Search" name="search">
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                        </form>
+                        session_start();
+
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "hub";
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        $sql = "
+                                SELECT * 
+                                FROM announcements
+                                ORDER BY date_created DESC";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            echo "<div class='cards'>";
+                            while($row = $result->fetch_assoc()) {  // Need to change this to only show 10-20 most recent announcements.
+
+                                $title=$row["title"];
+                                $content=$row["content"];
+                                $date_created=date("M d, Y",strtotime($row["date_created"]));
+                                $start_date=$row["start_date"];
+                                $end_date=$row["end_date"];
+                                // Need to add in name of instructor who posted announcement
+
+                                if($end_date>date("Y-m-d H:i:s")){
+                                    echo "
+                                        <div class='card'>
+                                            <div class='container'>
+                                                <h3><b>$title</b></h3> 
+                                                <p>$content</p> 
+                                                <h5>Posted: $date_created</h5>
+                                            </div>
+                                        </div>
+                                    ";
+                                }
+                            }
+                            echo "</div>";
+                        }
+                        $conn->close();
+                    ?>
+                </div>
+                <!-- End of Announcement Section -->
+
+                <!-- Calendar -->
+                <div class="col-sm-3">
+                    <div id="events">
+                        <h1>Events</h1>
+
+                        <!-- Everything below this line should be coded in php and pulled from
+                            the database.  I have it coded like this temporarily just so we can
+                            see what it will look like.
+
+                            TO-DO: Database needs new calendar table with month in text format, day in number format, and
+                            name of event as the fields.  Write query to pull the 3-5 events happening soonest from database
+                            and display below -->
+                        <div class="row calendar">
+                            <div class="col calendar-date">
+                                <div class="calendar-month">May</div>
+                                <div class="calendar-day">10</div>
+                            </div>
+                            <div class="col calendar-details">
+                                End of Semester
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row calendar">
+                            <div class="col calendar-date">
+                                <div class="calendar-month">May</div>
+                                <div class="calendar-day">11</div>
+                            </div>
+                            <div class="col calendar-details">
+                                Backyard Bash
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row calendar">
+                            <div class="col calendar-date">
+                                <div class="calendar-month">May</div>
+                                <div class="calendar-day">18</div>
+                            </div>
+                            <div class="col calendar-details">
+                                Graduation
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
-    </body>
-</html>
+                <!-- End of Calendar Section -->
+
+            </div> <!-- End of Row -->
+        </main>
+
+<?php
+    // Footer
+    include("./includes/footer.php");
+?>
---
+++ programs.php
@@ -0,0 +1,22 @@
+<?php
+// Header
+include("./includes/header.php");
+?>
+
+<!-- Main Content -->
+<main>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisl neque, iaculis in vehicula vitae, ornare ut nulla.
+        Phasellus scelerisque eget ex sit amet vulputate. Praesent quis tempor quam. Phasellus eget nisl in mi dictum euismod eget
+        quis dolor. Nam ac elit id mi posuere condimentum vel eget sem. Nullam cursus purus ut neque convallis pellentesque. Quisque
+        malesuada hendrerit bibendum. Donec volutpat quis erat vitae mollis. Praesent euismod ante eu tortor malesuada, at efficitur
+        ex congue. Pellentesque porta eu ipsum at tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Vestibulum non massa massa.
+    </p>
+
+</main>
+
+<?php
+// Footer
+include("./includes/footer.php");
+?>
